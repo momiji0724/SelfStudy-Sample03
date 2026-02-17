4f8c9ed8aae7d657317c834ef7e6ad2298fa9618
NOZOMIのPC\nozok <momiji0724@users.noreply.github.com>
2026-02-17 23:16:08 +0900

    20260217の自己学習

--- Assets/ItemSpwner.cs
+++ Assets/ItemSpwner.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ItemSpwner : MonoBehaviour
 {
     [SerializeField]
     private GameObject _prefab = null;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start() 
+    {
+        StartCoroutine(GetItemCreationIterator());
+    }
+    IEnumerator GetItemCreationIterator() 
     {
         for (var i = 0; i < 10; i++)
         {
+            yield return new WaitForSeconds(0.5f);
             var instance = Instantiate(_prefab);
             var x = Random.Range(-5.0f, 5.0f);
             var y = Random.Range(3.0f, 7.0f);
             var z = Random.Range(-5.0f, 5.0f);
             instance.transform.Translate(x, y, z);
+
+
+            yield return null;
         }
 
     }
 }
-    // Update is called once per frame
+
--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -10,14 +10,26 @@ public class UnityChanController : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         _animator.SetBool("Jump", false);
+        StartCoroutine(GetGameIterator());
     }
-
+    private bool _isGameOver = false;
+    IEnumerator GetGameIterator() 
+    {
+        yield return new WaitForSeconds(30);
+        _isGameOver = true;
+    }
     // Update is called once per frame
     void Update()
     {
+        if (_isGameOver) { return; }
         var v = Input.GetAxis("Vertical");
-        transform.Translate(0, 0, v * -0.05f);
+        transform.Translate(0, 0, v * 0.05f);
         _animator.SetFloat("Speed", v);
+
+        var h = Input.GetAxis("Horizontal");
+        transform.Rotate(0, h*2, 0);
+        _animator.SetFloat("Direction", h);
+
 
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
@@ -29,4 +41,39 @@ public class UnityChanController : MonoBehaviour
             _animator.SetBool("Jump",false);
         }
     }
+    private int _itemPoint = 0;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name != "Stage") 
+        {
+            _itemPoint += 10;
+            Destroy(collision.gameObject);
+        }
+        
+    }
+
+    private void OnGUI()
+    {
+        GUIStyle labelStyle = new GUIStyle();
+        labelStyle.fontSize = 40; // 文字の大きさ
+        labelStyle.normal.textColor = Color.white; // 文字の色
+
+        GUI.Label(new Rect(20, 20, 300, 50), "Point: " + _itemPoint, labelStyle);
+
+        if (_isGameOver)
+        {
+            GUIStyle centerStyle = new GUIStyle(labelStyle); // labelStyleをコピー
+            centerStyle.alignment = TextAnchor.MiddleCenter; // 中央揃えにする
+            centerStyle.fontSize = 80; // ゲームオーバーは少し大きく
+            centerStyle.normal.textColor = Color.red; // 赤色にする
+
+            float w = 400;
+            float h = 100;
+            float x = (Screen.width - w) / 2;
+            float y = (Screen.height - h) / 2;
+
+            GUI.Label(new Rect(x, y, w, h), "GameOver!", centerStyle);
+        }
+    }
 }
+
